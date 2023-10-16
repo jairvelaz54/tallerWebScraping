8c4cf61655df08cd2419a9e6c250f8d18522cbe0
Jair Velazquez <jairvelaz54.celaya@gmail.com>
2023-10-16 13:52:52 -0600

    se agrego 2 metodos de como extraer las imagenes

--- anime.php
+++ anime.php
@@ -4,11 +4,15 @@ include ('simple_html_dom.php');
 use Goutte\Client;
 $client=new Client();
 $html=connexion_pagina($client,"https://jkanime.net/directorio/",2);
-
+$content=connexion_pagina($client,"https://jkanime.net/directorio/",1);
 $d=extraer_titles($html);
 $p=extraer_descrip($html);
+$i=extraer_image($html);
+$i2=extraer_image_recorrido($content);
 var_dump($p);
 var_dump($d);
+var_dump($i);
+var_dump($i2);
 
 function connexion_pagina(Client $client, $url, $op){
     $peticion=$client->request("GET",$url);
@@ -43,3 +47,29 @@ function extraer_descrip($html){
     return $resultado;
 }
 
+
+function extraer_image($html){
+    $imagenes=array();
+    $imagen=$html->find('img.img-fluid.rounded-start');
+    foreach($imagen as $valor){
+        $imagenes[]=$valor->src;
+    }
+    return $imagenes;
+}
+function extraer_image_recorrido($content){
+    $images;
+    $img="https://cdn.jkdesu.com/assets/images/animes/image/";
+while(strpos($content, $img)){
+    $possible_url = substr($content, strpos($content, $img));
+    $pos_final = strpos($possible_url, '"');
+    $pos2_final = strpos($possible_url, "'");
+    if($pos2_final > 0 && $pos2_final < $pos_final){
+        $pos_final = $pos2_final;
+    }
+    $possible_url = substr($possible_url, 0, $pos_final);
+    
+    $content = substr($content, strpos($content, $img) + 1);
+     $images[]= $possible_url;
+}
+return $images;
+}
